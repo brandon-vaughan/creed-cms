f81e29ae2111130f8fcb2476f86c50c3e9270d67
Brandon Vaughan <brandonrvaughan@gmail.com>
2019-05-18 09:06:34 -0700

    Update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Creed Productions',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Creed Productions is a creative media firm that develops major motion picture screenplays and related entertainment.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
